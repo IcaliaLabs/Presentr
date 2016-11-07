cb834c84be578b5752ea3cac7f435866bf00dc2e
Daniel Lozano <danlozano@gmail.com>
2016-11-07 16:18:24 -0600

    Improved example project

--- PresentrExample/PresentrExample/AppDelegate.swift
+++ PresentrExample/PresentrExample/AppDelegate.swift
@@ -15,7 +15,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        UIApplication.shared.isStatusBarHidden = true
         return true
     }
 
