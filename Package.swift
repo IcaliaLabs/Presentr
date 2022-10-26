6050f065842f01f8196ad1b496c50d6d4deae85c
Tim Mikelj <timmikelj@monzo.com>
2022-10-26 14:40:59 +0100

    remove tests

--- Package.swift
+++ Package.swift
@@ -21,8 +21,5 @@ let package = Package(
         .target(
             name: "Presentr",
             dependencies: []),
-        .testTarget(
-            name: "PresentrTests",
-            dependencies: ["Presentr"]),
     ]
 )
